f1f5a5fca694d2a19b832b186a23332281ba76bd
praveensavinash <136537514+praveensavinash@users.noreply.github.com>
2024-01-30 16:43:40 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+noseX=0;
+noseY=0;
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/y84tjQPj/download.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+console.log('PoseNet is Initialized');
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    circle(noseX, noseY, 20);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+
+function gotPoses(result)
+{
+    if(results.length > 0)
+    {
+        console.log(result);
+        noseX = results[0].pose.nose.x;
+        nosey = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
